5cc8bb213680582a8c0830a14e176d81a0d894ea
Chris Underwood & Shannon Mulloy <student@epicodus.com>
2015-08-04 12:16:26 -0700

    added any case handling

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -9,4 +9,8 @@ describe('String#scrabble') do
   it('gives a scrabble score for a word') do
     expect("zebra".scrabble()).to(eq(16))
   end
+
+  it('gives a scrabble score for words in any case')  do
+    expect("eNcyclOpEdIa".scrabble()).to(eq(22))
+  end
 end
